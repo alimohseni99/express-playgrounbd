c40ab56b9f98d10a35e3e5dc0aba0e1bf8327369
Ali Mohseni <ali.mohseni@appliedtechnology.se>
2024-10-30 10:26:04 +0100

    first test with zod

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,5 +2,28 @@ import request from "supertest";
 import express from "express";
 import test from "node:test";
 import { z } from "zod";
-
-const userSchema = z.object({});
+const userSchema = z.object({
+  id: z.string().uuid(),
+  email: z.string().email(),
+});
+test("zod works!", () => {
+  const parseResult = userSchema.safeParse({ id: "abc-123", email: "me" });
+  if (!parseResult.success) {
+    return parseResult;
+  }
+});
+type User = z.infer<typeof userSchema>;
+test("supertest works!", () => {
+  const app = express();
+  app.get("/user", function (req, res) {
+    res.status(200).json({ name: "john" });
+  });
+  request(app)
+    .get("/user")
+    .expect("Content-Type", /json/)
+    .expect("Content-Length", "15")
+    .expect(200)
+    .end(function (err, res) {
+      if (err) throw err;
+    });
+});
